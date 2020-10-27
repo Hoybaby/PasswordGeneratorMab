04ab545ddfc97620ab0e8e89b987c6849ee05673
Hoybaby <mbartek436@gmail.com>
2020-10-26 23:07:12 -0400

    everything is working

--- script.js
+++ script.js
@@ -44,20 +44,21 @@ function generatePassword() {
 
     var start = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128"));
 
-    while (start <= 8 || start >= 129) {
+    while (start < 8 || start >= 129) {
         alert("Password needs a valid length. Please try again!");
        start = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128"));
     };
-    // if (!start) { 
-    //     alert("This needs a value");     
-    // } else if (start < 8 || start > 128) {
-    //     // Validates the user input
-    //     // now we will start the input for user
-    // } else {
-    //     confirmCharacter = confirm("Would you like Special Characters your password?");
-    //     confirmNumber = confirm("Would you like Numbers in your Password?");
-    //     confirmLowerCase = confirm("would you like Lower Case Letters in your Password?")
-    //     confirmUpperCase = confirm("Would you like Upper case letters in your Password?")
-    // }
+
+    if (!start) { 
+        alert("This needs a value");     
+    } else if (start < 8 || start > 128) {
+        // Validates the user input
+        // now we will start the input for user
+    } else {
+        confirmCharacter = confirm("Would you like Special Characters your password?");
+        confirmNumber = confirm("Would you like Numbers in your Password?");
+        confirmLowerCase = confirm("would you like Lower Case Letters in your Password?")
+        confirmUpperCase = confirm("Would you like Upper case letters in your Password?")
+    }
 
 };
