acccbb38f9f09886addd95f4c209e6f62b03a86e
Hoybaby <mbartek436@gmail.com>
2020-10-26 22:14:56 -0400

    trying to que an alert to varify my requirements for code but not working

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function writePassword() {
 
 }
 
-// possible values of code
+// Arrays of code
 
 var start;
 
@@ -18,44 +18,50 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 character = "!", "@", "#", "$", "%", "^", "&", "*", "()"
 
-upperCase = ["A","B","C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+UpperCase = ["A","B","C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+LowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var confirmSpecial = confirm("Would you like Special Characters your password?");
+var confirmCharacter;
 
-var confirmNumber = confirm("Would you like Numbers in your Password?");
+var confirmNumber;
 
-var confirmLowerCase = confirm("would you like Lower Case Letters in your Password?")
+var confirmLowerCase;
 
-var confirmUpperCase = confirm("Would you like Upper case letters in your Password?")
+var confirmUpperCase; 
 
 enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"))
 
 
 
 
-for (var i = 0; i <= generateBtn; i++); {
-  password = password + valuesString.charAt(Math.floor(Math.random() * Math.floor(value.length)))
-}
+// for (var i = 0; i <= generateBtn; i++); {
+//   password = password + valuesString.charAt(Math.floor(Math.random() * Math.floor(value.length)))
+// }
 
 function generatePassword() {
 
-    start = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128"));
+    start = prompt("How many characters would you like your password to be? Please choose between 8 and 128");
 
-    if (!start) { 
-        alert("This needs a value");     
-    } else if (start < 8 || start > 128) {
-        // Validates the user input
-        // now we will start the input for user
-    } else {
-        confirmSpecial =
-
+    while (start <= 8 || start >=129) {
+        alert("Password needs a valid length. Please try again!");
+        var start = prompt("How many characters would you like your password to be? Please choose between 8 and 128");
     }
+    // if (!start) { 
+    //     alert("This needs a value");     
+    // } else if (start < 8 || start > 128) {
+    //     // Validates the user input
+    //     // now we will start the input for user
+    // } else {
+    //     confirmCharacter = confirm("Would you like Special Characters your password?");
+    //     confirmNumber = confirm("Would you like Numbers in your Password?");
+    //     confirmLowerCase = confirm("would you like Lower Case Letters in your Password?")
+    //     confirmUpperCase = confirm("Would you like Upper case letters in your Password?")
+    // }
 
-}
+};
