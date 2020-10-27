4dd95fe50023625b8ce95e065f501f2eca453e1e
Hoybaby <mbartek436@gmail.com>
2020-10-26 21:33:28 -0400

    added the alphabets with strings

--- script.js
+++ script.js
@@ -15,10 +15,9 @@ confirmNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 confirmCharcter = "!", "@", "#", "$", "%", "^", "&", "*", "()"
 
-confirmUpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+confirmUpperCase = ["A","B","C", "D", "E", "F", "G", "H","I", "J", "K", "L", "M", "N", "O","P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-confirmLowerCase = "abcdefghijklmnopqrstuvwxyz"
-// valuesNumber 
+confirmLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 
