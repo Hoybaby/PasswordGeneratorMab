c16ece63a90a2b3755f8eafc4914d67036e94ef4
Hoybaby <mbartek436@gmail.com>
2020-10-28 19:33:28 -0400

    commented some uncessary code in attempt to make it cleaner and figure out a way to add the arrays together first

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -46,7 +49,7 @@ userPassword
 
 function generatePassword() {
 
-    var start = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128"));
+    var userInput = parseInt(prompt("How many characters would you like your password to be? Please choose between 8 and 128"));
 
     while (start < 8 || start >= 129) {
         alert("Password needs a valid length. Please try again!");
@@ -54,7 +57,7 @@ function generatePassword() {
     };
 
     // this will occur if user selects cancel on the choice
-    if (!start) {
+    if (!userInput) {
         alert("This needs a value");
     } else {
         confirmCharacter = confirm("Would you like Special Characters your password?");
@@ -62,8 +65,10 @@ function generatePassword() {
         confirmLowerCase = confirm("would you like Lower Case Letters in your Password?")
         confirmUpperCase = confirm("Would you like Upper case letters in your Password?")
     }
-
-    while (confirmCharacter === false && confirmNumber === false && confirmUpperCase === false && confirmLowerCase === false) {
+    
+        // FOR LOOP GOES HERE
+    }
+    if (confirmCharacter === false && confirmNumber === false && confirmUpperCase === false && confirmLowerCase === false) {
         alert("You must choose at least one!")
         confirmCharacter = confirm("Would you like Special Characters your password?");
         confirmNumber = confirm("Would you like Numbers in your Password?");
@@ -75,7 +80,7 @@ function generatePassword() {
     if (confirmCharacter === !true && confirmNumber === !true && confirmUpperCase === !true && confirmLowerCase === !true) {
         alert("You must choose at least one!")
        
-            // 4 options selected
+            // 4 options selected. 
     } else if(confirmCharacter === true && confirmNumber === true && confirmUpperCase === true && confirmLowerCase === true) {
         answers = confirmCharacter.concat(confirmNumber, confirmUpperCase, confirmLowerCase); 
 
@@ -118,16 +123,16 @@ function generatePassword() {
     }
 
 
-    var userPassword = []
+    // var userPassword = []
     
-    for (i = 0; i < start; i++) {
-        var genPass = answer[Math.floor(Math.random() * start.length)];
-        userPassword.push(genPass);
-    }
+    // for (i = 0; i < userInput; i++) {
+    //     var genPass = answers[Math.floor(Math.random() * start.length)];
+    //     userPassword.push(genPass);
+
 
     
-    return
-}
+    // return
+
 
 
 
