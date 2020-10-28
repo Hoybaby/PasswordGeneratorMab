3b14b984e120dc6cbd722c81781e08c1d4eec430
Hoybaby <mbartek436@gmail.com>
2020-10-27 22:40:27 -0400

    made a big mistake with not correctly estasblishing variables to my arrays so I did in this change

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ function writePassword() {
 
 var start;
 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-character = ["!", "@", "#", "$", "%", "^", "&", "*", "()"]
+var character = ["!", "@", "#", "$", "%", "^", "&", "*", "()"]
 
-UpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var UpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-LowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var LowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 // Add event listener to generate button
@@ -95,7 +95,7 @@ function generatePassword() {
         var genNumb = answers[Math.floor(Math.random() * answers.length)];
 
 
-    }
+    // }
 
     // var userSelections = {
     //     length: start,
@@ -133,6 +133,5 @@ function generatePassword() {
     //}
 
     //return the password array
-
+    }
 }
-
