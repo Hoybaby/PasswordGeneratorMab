1752c38c63150acae330c13af1febdedd8818e14
Hoybaby <mbartek436@gmail.com>
2020-10-28 21:50:36 -0400

    changed my arrays to actually whole string

--- script.js
+++ script.js
@@ -17,13 +17,11 @@ function writePassword() {
 
 var start;
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numbers = ("123456789")
 
-var character = ["!", "@", "#", "$", "%", "^", "&", "*", "()"]
-
-var UpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
-var LowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var character = ("!@#$%^&*()")
+var UpperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var LowerCase = ("abcdefghijklmnopqrstuvwxyz")
 
 
 // Add event listener to generate button
